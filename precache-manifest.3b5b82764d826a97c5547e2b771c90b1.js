8afed7d4001e546d4d374d6b9ec1bdb228bdb018
Jeremy Bratcher <jeremybratcher@gmail.com>
2019-02-14 12:01:23 +0000

    Updates

---
+++ precache-manifest.3b5b82764d826a97c5547e2b771c90b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3029aedfd15bcf830b8c",
+    "url": "/court-designer/static/js/runtime~main.3029aedf.js"
+  },
+  {
+    "revision": "b75d0fb884204050c4df",
+    "url": "/court-designer/static/js/main.b75d0fb8.chunk.js"
+  },
+  {
+    "revision": "815297a0692c18394c82",
+    "url": "/court-designer/static/js/1.815297a0.chunk.js"
+  },
+  {
+    "revision": "b75d0fb884204050c4df",
+    "url": "/court-designer/static/css/main.fdfd41e4.chunk.css"
+  },
+  {
+    "revision": "80d7fdcab4ed234a6b054ac0ff452c7b",
+    "url": "/court-designer/index.html"
+  }
+];
