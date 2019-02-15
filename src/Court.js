4f6a5bcc2d2f76928dac27edfeb937b79d1afe33
Jeremy Bratcher <jeremybratcher@gmail.com>
2019-02-15 12:28:59 +0000

    fix: repositioned border lettering (again) with respect to court border

--- src/Court.js
+++ src/Court.js
@@ -22,6 +22,17 @@ class Court extends Component {
 
         <section className="gym center">
 
+            <section
+              className="courtBorderLine center"
+              style={{
+                borderColor: borderColor,
+                borderTop: `${borderSideSize}px solid ${borderColor}`,
+                borderBottom: `${borderSideSize}px solid ${borderColor}`,
+                borderLeft: `${borderEndSize}px solid ${borderColor}`,
+                borderRight: `${borderEndSize}px solid ${borderColor}`,
+              }}
+            >
+            
             <section className="leftEndBorderLettering center">
 
                 <span
@@ -33,218 +44,6 @@ class Court extends Component {
                 >
                   {leftEndBorderText}
                 </span>
-
-            </section>
-
-            
-
-            <section
-              className="courtBorderLine center"
-              style={{
-                borderColor: borderColor,
-                borderTop: `${borderSideSize}px solid ${borderColor}`,
-                borderBottom: `${borderSideSize}px solid ${borderColor}`,
-                borderLeft: `${borderEndSize}px solid ${borderColor}`,
-                borderRight: `${borderEndSize}px solid ${borderColor}`,
-              }}
-            >
-
-              <section
-                className="courtOuterGameLine center"
-                style={{boxShadow: `inset 0 0 0 0.25rem ${mainGameLineColor}`}}
-              >
-
-                <section
-                  className="courtFloor"
-                >
-
-                  {/* Left Court Area */}
-
-                  <section
-                    className="leftCourtArea center"
-                    style={{
-                      borderColor: mainGameLineColor,
-                      width: `calc(378px + ${borderEndSize})`
-                    }}
-                  >
-
-                    <section
-                      className="leftLaneArea"
-                      style={{borderColor: mainGameLineColor}}
-                    >
-
-                      <ul className="leftLaneMarkingContainer">
-
-                        <li
-                          className="leftLaneTopMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-                        <li
-                          className="leftLaneTopMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-                        <li
-                          className="leftLaneTopMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-                        <li
-                          className="leftLaneTopMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-
-                      </ul>
-
-                      <ul className="leftLaneMarkingContainer">
-
-                        <li
-                          className="leftLaneBottomMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-                        <li
-                          className="leftLaneBottomMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-                        <li
-                          className="leftLaneBottomMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-                        <li
-                          className="leftLaneBottomMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-
-                      </ul>
-
-                      <section className="leftGoalContainer">
-
-                        <section className="leftGoalBackboard"></section>
-                        <section className="leftGoalMount"></section>
-                        <section className="leftGoalRim"></section>
-
-                      </section>
-
-                    </section>
-
-                    <section
-                      className="leftKeyArea"
-                      style={{borderColor: mainGameLineColor}}
-                    >
-
-                    </section>
-
-                    <section
-                      className="leftThreePointArea"
-                      style={{borderColor: mainGameLineColor}}
-                    >
-
-                    </section>
-
-                  </section>
-
-                  {/* Center Court Area */}
-
-                  <section
-                    className="centerCourtLine center"
-                    style={{background: mainGameLineColor}}
-                  >
-
-                    <section
-                      className="centerCircle center"
-                      style={{borderColor: mainGameLineColor}}
-                    >
-
-                    </section>
-
-                  </section>
-
-                  {/* Right Court Area */}
-
-                  <section
-                    className="rightCourtArea center"
-                    style={{
-                      borderColor: mainGameLineColor,
-                      width: `calc(378px + ${borderEndSize})`
-                    }}
-                  >
-
-                    <section
-                      className="rightThreePointArea"
-                      style={{borderColor: mainGameLineColor}}
-                    >
-
-                    </section>
-
-                    <section
-                      className="rightKeyArea"
-                      style={{borderColor: mainGameLineColor}}
-                    >
-
-                    </section>
-
-                    <section
-                      className="rightLaneArea"
-                      style={{borderColor: mainGameLineColor}}
-                    >
-
-                      <ul
-                        className="rightLaneMarkingContainer"
-                      >
-
-                        <li
-                          className="rightLaneTopMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-                        <li
-                          className="rightLaneTopMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-                        <li
-                          className="rightLaneTopMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-                        <li
-                          className="rightLaneTopMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-
-                      </ul>
-
-                      <ul className="rightLaneMarkingContainer">
-
-                        <li
-                          className="rightLaneBottomMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-                        <li
-                          className="rightLaneBottomMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-                        <li
-                          className="rightLaneBottomMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-                        <li
-                          className="rightLaneBottomMarkings"
-                          style={{background: mainGameLineColor}}
-                        ></li>
-
-                      </ul>
-
-                      <section className="rightGoalContainer">
-
-                        <section className="rightGoalRim"></section>
-                        <section className="rightGoalMount"></section>
-                        <section className="rightGoalBackboard"></section>
-
-                      </section>
-
-                    </section>
-
-                  </section>
-
-                </section>
-
-              </section>
 
             </section>
             
@@ -261,6 +60,205 @@ class Court extends Component {
                 </span>
 
             </section>
+
+            <section
+              className="courtOuterGameLine center"
+              style={{boxShadow: `inset 0 0 0 0.25rem ${mainGameLineColor}`}}
+            >
+
+              <section
+                className="courtFloor"
+              >
+
+                {/* Left Court Area */}
+
+                <section
+                  className="leftCourtArea center"
+                  style={{
+                    borderColor: mainGameLineColor,
+                    width: `calc(378px + ${borderEndSize})`
+                  }}
+                >
+
+                  <section
+                    className="leftLaneArea"
+                    style={{borderColor: mainGameLineColor}}
+                  >
+
+                    <ul className="leftLaneMarkingContainer">
+
+                      <li
+                        className="leftLaneTopMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+                      <li
+                        className="leftLaneTopMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+                      <li
+                        className="leftLaneTopMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+                      <li
+                        className="leftLaneTopMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+
+                    </ul>
+
+                    <ul className="leftLaneMarkingContainer">
+
+                      <li
+                        className="leftLaneBottomMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+                      <li
+                        className="leftLaneBottomMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+                      <li
+                        className="leftLaneBottomMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+                      <li
+                        className="leftLaneBottomMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+
+                    </ul>
+
+                    <section className="leftGoalContainer">
+
+                      <section className="leftGoalBackboard"></section>
+                      <section className="leftGoalMount"></section>
+                      <section className="leftGoalRim"></section>
+
+                    </section>
+
+                  </section>
+
+                  <section
+                    className="leftKeyArea"
+                    style={{borderColor: mainGameLineColor}}
+                  >
+
+                  </section>
+
+                  <section
+                    className="leftThreePointArea"
+                    style={{borderColor: mainGameLineColor}}
+                  >
+
+                  </section>
+
+                </section>
+
+                {/* Center Court Area */}
+
+                <section
+                  className="centerCourtLine center"
+                  style={{background: mainGameLineColor}}
+                >
+
+                  <section
+                    className="centerCircle center"
+                    style={{borderColor: mainGameLineColor}}
+                  >
+
+                  </section>
+
+                </section>
+
+                {/* Right Court Area */}
+
+                <section
+                  className="rightCourtArea center"
+                  style={{
+                    borderColor: mainGameLineColor,
+                    width: `calc(378px + ${borderEndSize})`
+                  }}
+                >
+
+                  <section
+                    className="rightThreePointArea"
+                    style={{borderColor: mainGameLineColor}}
+                  >
+
+                  </section>
+
+                  <section
+                    className="rightKeyArea"
+                    style={{borderColor: mainGameLineColor}}
+                  >
+
+                  </section>
+
+                  <section
+                    className="rightLaneArea"
+                    style={{borderColor: mainGameLineColor}}
+                  >
+
+                    <ul
+                      className="rightLaneMarkingContainer"
+                    >
+
+                      <li
+                        className="rightLaneTopMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+                      <li
+                        className="rightLaneTopMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+                      <li
+                        className="rightLaneTopMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+                      <li
+                        className="rightLaneTopMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+
+                    </ul>
+
+                    <ul className="rightLaneMarkingContainer">
+
+                      <li
+                        className="rightLaneBottomMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+                      <li
+                        className="rightLaneBottomMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+                      <li
+                        className="rightLaneBottomMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+                      <li
+                        className="rightLaneBottomMarkings"
+                        style={{background: mainGameLineColor}}
+                      ></li>
+
+                    </ul>
+
+                    <section className="rightGoalContainer">
+
+                      <section className="rightGoalRim"></section>
+                      <section className="rightGoalMount"></section>
+                      <section className="rightGoalBackboard"></section>
+
+                    </section>
+
+                  </section>
+
+                </section>
+
+              </section>
+
+            </section>
+
+          </section>
 
         </section>
 
